2caf32f1e911df430580e8665f7034f602c052d2
Jeff Dodson <jeffdod@gmail.com>
2024-02-24 11:08:40 -0600

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DragAndDropTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
